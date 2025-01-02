9eaaf724dc0d23d218825f37b8cbbb2548fdc4bb
Sebastian barrera <sebasbh2008@hotmail.com>
2025-01-02 18:55:58 -0500

    feat: ✨: Add class base abstract

---
+++ src/models/Cream.java
@@ -0,0 +1,34 @@
+package models;
+
+public abstract class Cream {
+    public String client;
+
+    public final void preparation () {
+        pourMilk();
+        pourEggs();
+        essence();
+        fruitEssence();
+        freeze();
+        finish();
+        System.out.println("Tu crema esta lista, disfrutala!");
+    }
+
+    private void pourMilk () {
+        System.out.println("Echar dos tazas de leche y azucar en un recipiente");
+    }
+
+    private void pourEggs() {
+        System.out.println("Revolver por dos minutos y echar 4 yemas de huevo");
+    }
+
+    protected abstract void essence();
+
+    protected abstract void fruitEssence();
+
+    private void freeze(){
+        System.out.println("Cocinar bajo fuego bajo y revolver contantmente hasta que espese y tenga una textura suave. Llevar recipiente al congelador y dejar por dos horas");
+    }
+
+    protected abstract void finish();
+
+}
